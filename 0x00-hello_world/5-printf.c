b6cdd864d3d32208784ac0928c6b99cee079c2ff
Allan Kamau <allanrittol@outlook.com>
2023-07-13 19:37:18 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  *  Main - this is the main function
  *
- *   Return: 0
+ *   Return: always 0
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
