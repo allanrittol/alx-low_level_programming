0a9af8a1a599a573085cc25dca982152f5b279f6
Allan Kamau <allanrittol@outlook.com>
2023-07-15 06:52:54 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /*
  * main - A C program that prints a line to a standard error
  *
- * Return: 1
+ * Return: 1 (success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
