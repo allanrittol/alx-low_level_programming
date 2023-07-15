72f8d33547f70467b33172bbbc5ae78e7b0e42d3
Allan Kamau <allanrittol@outlook.com>
2023-07-15 06:43:25 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /*
- * main - This is the main function
+ * main - A C program that prints a line to a standard error
  *
  * Return: 1
  */
-
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
 	return (1);
 }
