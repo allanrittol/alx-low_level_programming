52ae7c935b04a6557ffb64f2915bf0572f64829f
Allan Kamau <allanrittol@outlook.com>
2023-07-16 18:22:44 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,16 +4,21 @@
  *
  * Return: always 0
  */
+
 int main(void)
 {
 	int a;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0 ; a < 10 ; a++)
 	{
-		putchar(a + '0');
-		putchar(',');
-		putchar(' ');
+		if (a == 9)
+			putchar(a + '0');
+		else
+		{
+			putchar(a + '0');
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	putchar('\n');
 	return (0);
 }
