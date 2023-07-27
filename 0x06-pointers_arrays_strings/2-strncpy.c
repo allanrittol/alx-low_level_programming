5c402d2484fbe654399902f0fe1cecf6df77880a
Allan Kamau <allanrittol@outlook.com>
2023-07-27 09:51:11 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,8 +4,8 @@
  * _strncpy - main function that copies a string
  * @dest: destination string
  * @src: source string
- * Return: dest
  * @n: number of bytes
+ * Return: dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
