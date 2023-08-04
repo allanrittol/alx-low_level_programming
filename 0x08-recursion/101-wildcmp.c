524ea2b9eb7f16eaedc15c267165b97330983b9f
Allan Kamau <allanrittol@outlook.com>
2023-08-04 07:40:11 +0000

    101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * wildcmp- function compares two strings
+ * @s1: pointer to string 1
+ * @s2: pointer to string 2
+ * Return: 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s)
+}
