400a5519ab08a1d084e53afb3a70617556652162
Allan Kamau <allanrittol@outlook.com>
2023-08-08 18:43:56 +0000

    a program that multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the nuktiplication of two numbers
+ * @argc: calcualtes the numbers to be multipplied
+ * @argv: contains the arrrays
+ * Return: (1) if successfully returns an error
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
+}
