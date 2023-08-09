3aaf6feba98a5cd68adaf140493603e91a68069e
Allan Kamau <allanrittol@outlook.com>
2023-08-09 05:01:47 +0000

    a progrma that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,7 +6,7 @@
  * main - prints the sum of positive numbers followed by a new line
  * @argc: calculates the sum of numbers followed by a new line
  * @argv: contains the arrays to be calculated from
- * Return: (1) if scuccessful, if not successful print error
+ * Return: () if scuccessful, if not successful print error
  */
 
 int main(int argc, char *argv[])
@@ -14,15 +14,9 @@ int main(int argc, char *argv[])
 	int i, j;
 	int total = 0;
 
-	if (argc <= 1)
-	{
-		printf("0\n");
-		return (0);
-	}
-
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '0'; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (!isdigit(argv[i][j]))
 			{
