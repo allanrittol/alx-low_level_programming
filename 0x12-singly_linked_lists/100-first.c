c8c500515dc65e5cecf9cb8177bd92af30c55992
Allan Kamau <allanrittol@outlook.com>
2023-08-29 06:23:02 +0000

    a function that prints You're beat! and yet, you must allow,\nI bore my house upon my back!

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * printFirst - function that prints a sentence
+ *
+ */
+
+void printFirst(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
