fe5f22a03962ad3136bae955d750c1fbe4b0e28e
Allan Kamau <allanrittol@outlook.com>
2023-08-31 07:23:57 +0000

    a program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,10 +9,10 @@
  *
  * Return: always (0)
  */
-int main(int __attribute__((__unused__))argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int num1, num2;
-	char *op;
+	int num1 = 0, num2 = 0, res = 0;
+	int (*op)(int, int);
 
 	if (argc != 4)
 	{
@@ -21,22 +21,22 @@ int main(int __attribute__((__unused__))argc, char *argv[])
 	}
 
 	num1 = atoi(argv[1]);
-	op = argv[2];
 	num2 = atoi(argv[3]);
 
-	if (get_op_func(op) == NULL || op[1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	if ((*argv[2] == '/' || *argv[2] == '%') && (*argv[3] == '0'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", get_op_fun(op) (num1,num2));
+	op = get_op_func(argv[2]);
+	if (op == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	res = op(num1, num2);
+	printf("%d\n", res);
 	return (0);
 
 }
