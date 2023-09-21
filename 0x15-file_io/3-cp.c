54b14dd63d26c185b1f49d3bb38f2f432adab9e3
Allan Kamau <allanrittol@outlook.com>
2023-09-21 10:00:32 +0000

    File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,72 +3,91 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define BUF_SIZE 1024
 /**
- * error_file - function copies the content for another file
- * @file_from: where the file is copied from
- * @file_to: where the file is copied to
- * @argv: argument vector
- * Return: 0 if successful
- */
-void error_file(int file_from, int file_to, char *argv[])
+* main - copies the content of one file to another
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: Always 0
+**/
+
+int main(int argc, char **argv)
 {
-	if (file_from == -1)
+	int fd, fd1, src0, src1;
+	char *buffer;
+
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	buffer = malloc(sizeof(char) * BUF_SIZE);
+	if (!buffer)
+		return (0);
+
+	fd1 = open(argv[1], O_RDONLY);
+	error_98(fd1, buffer, argv[1]);
+	fd = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	error_99(fd, buffer, argv[2]);
+	do {
+		src0 = read(fd1, buffer, BUF_SIZE);
+		if (src0 == 0)
+			break;
+		error_98(src0, buffer, argv[1]);
+		src1 = write(fd, buffer, src0);
+		error_99(src1, buffer, argv[2]);
+	} while (src1 >= BUF_SIZE);
+	src0 = close(fd);
+	error_100(src0, buffer);
+	src0 = close(fd1);
+	error_100(src0, buffer);
+	free(buffer);
+	return (0);
+}
+
+/**
+* error_98 - checks error 98
+* @fd: the value to check
+* @buffer: the buffer
+* @argv: argument
+**/
+void error_98(int fd, char *buffer, char *argv)
+{
+
+	if (fd < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv);
+		free(buffer);
 		exit(98);
 	}
+}
 
-	if (file_to == -1)
+/**
+* error_99 - checks error 99
+* @fd: value to check
+* @buffer: the buffer to be read from
+* @argv: argument
+*/
+void error_99(int fd, char *buffer, char *argv)
+{
+	if (fd < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv);
+		free(buffer);
 		exit(99);
 	}
 }
 /**
- * main - function copies the content for another file
- * @argc: the number of arguments supplied
- * @argv: an array of the pointers of the args supplied
- *
- * Return: 0 if successful
- */
-int main(int argc, char *argv[])
+* error_100 - checks error 100
+* @fd: the value to check
+* @buffer: the buffer
+*/
+void error_100(int fd, char *buffer)
 {
-	int file_from, file_to, error_term;
-	ssize_t nchars, nwr;
-	char buf[1024];
-
-	if (argc != 3)
+	if (fd < 0)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-		exit(97);
-	}
-
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(file_from, file_to, argv);
-
-	nchars = 1024;
-	while (nchars == 1024)
-	{
-		nchars = read(file_from, buf, 1024);
-		if (nchars == -1)
-			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
-		if (nwr == -1)
-			error_file(0, -1,argv);
-	}
-
-	error_term = close(file_from);
-	if (error_term == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd);
+		free(buffer);
 		exit(100);
 	}
-	error_term = close(file_to);
-	if (error_term == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
-	}
-	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,11 +6,17 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-
+int _putchar(char c);
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
+int main(int argc, char **argv);
+void error_98(int fd, char *buffer, char *argv);
+void error_99(int fd, char *buffer, char *argv);
+void error_100(int fd, char *buffer);
 
 
 #endif
