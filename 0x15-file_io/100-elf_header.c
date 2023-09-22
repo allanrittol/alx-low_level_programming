6dd145e4ff0cd34598a5333da4fd5cd57a7b293b
Allan Kamau <allanrittol@outlook.com>
2023-09-22 13:57:50 +0000

    elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,10 +1,27 @@
 #include "main.h"
 #include <elf.h>
 /**
-* main - main
+* check_elf - function that checks if a file is an ELF file
+* @e_identity: pointer to an array containing the ELF magic numbers
+*
 * Return: always 0
 */
+void check_elf(unsigned char *e_indentity)
+{
+	int index;
 
+	for (index = 0; index < 4; index++)
+	{
+		if (e_identity[index] != 127 &&
+			e_identity[index] != 'E' &&
+			e_identity[index] != 'L' &&
+			e_identity[index] != 'F')
+		{
+			dprint(STDERR_FILENO, "Error: Not an ELF file\n");
+			exit(98);
+		}
+	}
+}
 int main(void)
 {
 
